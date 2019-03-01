6cd7c6991254d62ab7092c6c0d14953c2cade563
Padraig <pawdrik@hotmail.com>
2019-03-01 15:01:45 +0000

    Version 6

--- Project/routes/index.js
+++ Project/routes/index.js
@@ -42,29 +42,60 @@ mongoose.connect ('mongodb://admin:admin2019@ds251894.mlab.com:51894/final');
 
 var options = {
 
-    url: 'https://api-v3.igdb.com/games/?fields=name,cover.url,genres.name; sort popularity desc',
+    url: 'https://api-v3.igdb.com/games/?fields=aggregated_rating,name,cover.url,genres.name,total_rating,videos.name; sort popularity desc' ,
     headers: {
         "user-key":"47a6def808445c928fc853ff4dc8b30d"
     },
     dataType:"jsonp",
 };
 
+/*var options2 = {
+
+    url: 'https://api-v3.igdb.com/reviews/?fields=url,game,content; sort popularity desc',
+    headers: {
+        "user-key":"47a6def808445c928fc853ff4dc8b30d"
+    },
+    dataType:"jsonp",
+};*/
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    request(options, (error, response) => {
+    request(options,(error, response) => {
         if (error) {
             res.sendStatus(504);
         } else {
             let games = JSON.parse(response.body);
             console.log(response.data);
             res.render('index', { title: 'Hello World', user: req.user, games:games});
+        }
+    })
+});
+
+
+/*var options2 = {
+
+    url: 'https://api-v3.igdb.com/games/?fields=name,release_date,reviews',
+    headers: {
+        "user-key":"47a6def808445c928fc853ff4dc8b30d"
+    },
+    dataType:"jsonp",
+};*/
+
+router.post('/games/:id', function(req, res, next) {
+
+    request(options2, (error, response) => {
+        if (error) {
+            res.sendStatus(504);
+        } else {
+            let games1 = JSON.parse(response.body);
+            console.log(response.data);
+            res.render('views/games/_id', { games1:games1});
 
 
 
         }
     })
-})
-
+});
 
 
 
